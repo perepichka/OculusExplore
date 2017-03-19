42140510f953a37ebcb735d592e03373ed0a72a6
maxedoutstudio <max@maxedoutstudio.com>
2017-03-19 18:28:03 -0400

    More initialization commits
    Fixed gitignore.

---
+++ Assets/Scripts/ConfigParser.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConfigParser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
